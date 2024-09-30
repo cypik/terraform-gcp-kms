2f3e05d4f114c806cfcf712ece2d4f89a8d20b9a
cypiksuresh <suresh.y@cypik.com>
2024-09-30 14:22:59 +0530

    update resource and readme.md

--- main.tf
+++ main.tf
@@ -1,14 +1,27 @@
+data "google_client_config" "current" {}
+
 module "labels" {
   source      = "cypik/labels/google"
-  version     = "1.0.1"
+  version     = "1.0.2"
   name        = var.name
   environment = var.environment
   label_order = var.label_order
   managedby   = var.managedby
   repository  = var.repository
+  extra_tags  = var.extra_tags
 }
 
-data "google_client_config" "current" {
+locals {
+  keys_list = ["key1"]
+
+  # Determine the KMS key IDs based on whether to prevent destruction
+  keys_ids = var.prevent_destroy ? google_kms_crypto_key.key[*].id : google_kms_crypto_key.key_ephemeral[*].id
+
+  # Ensure we have enough IDs
+  keys_ids_final = length(local.keys_ids) == 0 ? [for _ in local.keys_list : ""] : local.keys_ids
+
+  # Create a map of keys by name with corresponding IDs
+  keys_by_name = zipmap(local.keys_list, local.keys_ids_final)
 }
 
 #####==============================================================================
@@ -25,48 +38,81 @@ resource "google_kms_key_ring" "key_ring" {
 # A CryptoKey represents logical key that can be used for cryptographic operations.
 #####==============================================================================
 resource "google_kms_crypto_key" "key" {
-  count           = var.kms_crypto_key_enabled && var.enabled ? 1 : 0
-  name            = format("%s-key", module.labels.id)
-  key_ring        = join("", google_kms_key_ring.key_ring[*].id)
-  rotation_period = var.key_rotation_period
-  purpose         = var.purpose
+  count                         = var.prevent_destroy ? 1 : 0 # Create only one key if not preventing destruction
+  name                          = format("%s-key", module.labels.id)
+  key_ring                      = google_kms_key_ring.key_ring[0].id
+  rotation_period               = var.key_rotation_period
+  purpose                       = var.purpose
+  import_only                   = var.import_only
+  skip_initial_version_creation = var.skip_initial_version_creation
+  crypto_key_backend            = var.crypto_key_backend
 
   lifecycle {
     prevent_destroy = false
   }
 
+  destroy_scheduled_duration = var.key_destroy_scheduled_duration
+
   version_template {
     algorithm        = var.key_algorithm
     protection_level = var.key_protection_level
   }
-}
 
+  labels = var.labels
+}
 #####==============================================================================
 # A CryptoKey represents logical key that can be used for cryptographic operations.
 #####==============================================================================
 resource "google_kms_crypto_key" "key_ephemeral" {
-  count           = var.kms_crypto_key_enabled && var.enabled ? 1 : 0
-  name            = format("%s-cryptokey", module.labels.id)
-  key_ring        = join("", google_kms_key_ring.key_ring[*].id)
-  rotation_period = var.key_rotation_period
-  purpose         = var.purpose
+  count                         = var.prevent_destroy ? 1 : 0
+  name                          = format("%s-cryptokey", module.labels.id)
+  key_ring                      = google_kms_key_ring.key_ring[0].id
+  rotation_period               = var.key_rotation_period
+  purpose                       = var.purpose
+  import_only                   = var.import_only
+  skip_initial_version_creation = var.skip_initial_version_creation
+  crypto_key_backend            = var.crypto_key_backend
 
   lifecycle {
     prevent_destroy = false
   }
 
+  destroy_scheduled_duration = var.key_destroy_scheduled_duration
+
   version_template {
     algorithm        = var.key_algorithm
     protection_level = var.key_protection_level
   }
+
+  labels = var.labels
 }
 
 #####==============================================================================
 #####Three different resources help you manage your IAM policy for KMS crypto key.
 #####==============================================================================
 resource "google_kms_crypto_key_iam_binding" "owners" {
-  count         = var.kms_crypto_key_iam_binding_enabled && var.enabled ? 1 : 0
-  role          = var.role
-  crypto_key_id = join("", google_kms_crypto_key.key[*].id)
-  members       = compact(split(",", var.service_accounts[count.index]))
+  count = length(var.set_owners_for) > 0 && length(local.keys_by_name) >= length(var.set_owners_for) ? length(var.set_owners_for) : 0
+
+  role          = "roles/owner"
+  crypto_key_id = lookup(local.keys_by_name, var.set_owners_for[count.index], null)
+
+  members = length(var.owners) > count.index ? compact(split(",", var.owners[count.index])) : []
+}
+
+resource "google_kms_crypto_key_iam_binding" "decrypters" {
+  count = length(var.set_decrypters_for) > 0 && length(local.keys_by_name) >= length(var.set_decrypters_for) ? length(var.set_decrypters_for) : 0
+
+  role          = "roles/cloudkms.cryptoKeyDecrypter"
+  crypto_key_id = lookup(local.keys_by_name, var.set_decrypters_for[count.index], null)
+
+  members = length(var.decrypters) > count.index ? compact(split(",", var.decrypters[count.index])) : []
+}
+
+resource "google_kms_crypto_key_iam_binding" "encrypters" {
+  count = length(var.set_encrypters_for) > 0 && length(local.keys_by_name) >= length(var.set_encrypters_for) ? length(var.set_encrypters_for) : 0
+
+  role          = "roles/cloudkms.cryptoKeyEncrypter"
+  crypto_key_id = lookup(local.keys_by_name, var.set_encrypters_for[count.index], null)
+
+  members = length(var.encrypters) > count.index ? compact(split(",", var.encrypters[count.index])) : []
 }
--- variables.tf
+++ variables.tf
@@ -11,15 +11,21 @@ variable "environment" {
 }
 
 variable "label_order" {
-  type        = list(any)
+  type        = list(string)
   default     = ["name", "environment"]
   description = "Label order, e.g. sequence of application name and environment `name`,`environment`,'attribute' [`webserver`,`qa`,`devops`,`public`,] ."
 }
 
+variable "extra_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags for the resource."
+}
+
 variable "managedby" {
   type        = string
-  default     = "cypik"
-  description = "ManagedBy, eg 'cypik'."
+  default     = "info@cypik.com"
+  description = "ManagedBy, eg 'info@cypik.com'"
 }
 
 variable "repository" {
@@ -42,26 +48,8 @@ variable "enabled" {
 
 variable "location" {
   type        = string
-  default     = "asia"
+  default     = ""
   description = "Location for the keyring."
-}
-
-variable "service_accounts" {
-  type        = list(string)
-  default     = []
-  description = "List of comma-separated owners for each key declared in set_owners_for."
-}
-
-variable "kms_crypto_key_enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to prevent the module from creating any resources."
-}
-
-variable "kms_crypto_key_iam_binding_enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to prevent the module from creating any resources."
 }
 
 variable "key_algorithm" {
@@ -88,8 +76,74 @@ variable "purpose" {
   description = "The immutable purpose of the CryptoKey. Possible values are ENCRYPT_DECRYPT, ASYMMETRIC_SIGN, and ASYMMETRIC_DECRYPT."
 }
 
-variable "role" {
+variable "prevent_destroy" {
+  description = "Set the prevent_destroy lifecycle attribute on keys."
+  type        = bool
+  default     = true
+}
+
+variable "key_destroy_scheduled_duration" {
+  description = "Set the period of time that versions of keys spend in the DESTROY_SCHEDULED state before transitioning to DESTROYED."
   type        = string
-  default     = ""
-  description = "this role use for permissions"
+  default     = null
+}
+
+variable "set_owners_for" {
+  description = "Name of keys for which owners will be set."
+  type        = list(string)
+  default     = []
+}
+
+variable "owners" {
+  description = "List of comma-separated owners for each key declared in set_owners_for."
+  type        = list(string)
+  default     = []
+}
+
+variable "set_encrypters_for" {
+  description = "Name of keys for which encrypters will be set."
+  type        = list(string)
+  default     = []
+}
+
+variable "encrypters" {
+  description = "List of comma-separated owners for each key declared in set_encrypters_for."
+  type        = list(string)
+  default     = []
+}
+
+variable "set_decrypters_for" {
+  description = "Name of keys for which decrypters will be set."
+  type        = list(string)
+  default     = []
+}
+
+variable "decrypters" {
+  description = "List of comma-separated owners for each key declared in set_decrypters_for."
+  type        = list(string)
+  default     = []
+}
+
+variable "labels" {
+  type        = map(string)
+  description = "Labels, provided as a map"
+  default     = {}
+}
+
+variable "import_only" {
+  type        = bool
+  description = "Whether these keys may contain imported versions only."
+  default     = false
+}
+
+variable "skip_initial_version_creation" {
+  type        = bool
+  description = "If set to true, the request will create CryptoKeys without any CryptoKeyVersions."
+  default     = false
+}
+
+variable "crypto_key_backend" {
+  type        = string
+  description = "(Optional) The resource name of the backend environment associated with all CryptoKeyVersions within this CryptoKey. The resource name is in the format 'projects//locations//ekmConnections/*' and only applies to 'EXTERNAL_VPC' keys."
+  default     = null
 }
